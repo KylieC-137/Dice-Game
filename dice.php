35c81b131bc74c2d219ac316c77002b208ed79b5
KylieC-137 <kydalessandro@gmail.com>
2018-10-22 16:39:45 -0400

    Update dice.php

--- dice.php
+++ dice.php
@@ -11,15 +11,3 @@ $second = roll();
 <h3><?php echo "$first  +  $second = ";
 echo $first + $second;
 ?></h3>
-
-<?php
-$words = array (
-	"giants"
-	"triangle"
-	"particle"
-);
-$letters = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
-'p','q','r','s','t','u','v','w','x','y','z');
-$right = array_fill_keys($letters, '.');
-	
-?>
